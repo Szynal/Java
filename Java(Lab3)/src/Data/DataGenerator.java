0c7ac4f7c7c9d622238d1e8ed519198b5cf6000a
Paweł Szynal <pawel.szynal@email.com>
2018-03-19 16:29:49 +0100

    DataGenerator added

---
+++ Java(Lab3)/src/Data/DataGenerator.java
@@ -0,0 +1,52 @@
+package Data;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+/**
+ * The class responsible for generating the List of Double objects.
+ * 
+ * @author Pawel Szynal
+ *
+ */
+public class DataGenerator {
+
+	/**
+	 * The smallest list size possible to generate. (10^6)
+	 */
+	private static int minListSize = 1000000;
+	/**
+	 * The largest list size possible to generate (10^9)
+	 */
+	private static int maxListSize = 100000000;
+
+	/**
+	 * <p>
+	 * GenerateList
+	 * </p>
+	 * Creates a new random List generator using a single long seed. T
+	 * 
+	 * @param seed
+	 *            the seed is the initial value of the internal state of the
+	 *            pseudorandom number generator which is maintained by method
+	 *            next(int).
+	 * @param minValue
+	 *            The smallest list value possible to generate.
+	 * @param maxValue
+	 *            The largest list value possible to generate.
+	 * @return The generated list.
+	 */
+	public static List<Double> GenerateList(long seed, int minValue, int maxValue) {
+		List<Double> list = new ArrayList<Double>();
+		Random random = new Random(seed);
+
+		int size = random.nextInt((maxListSize - minListSize) + 1) + minListSize;
+
+		for (int i = 0; i < size; i++)
+			list.add(minValue + (maxValue - minValue) * random.nextDouble());
+
+		return list;
+	}
+
+}
